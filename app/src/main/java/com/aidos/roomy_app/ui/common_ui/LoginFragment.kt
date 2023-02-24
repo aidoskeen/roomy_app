303e887daff6a0b7eb3a6f263d5b87cadfbaf543
Aidos Alimkhan <aidos.alimkhan@ashburn.eu>
2023-02-24 09:06:33 +0200

    Login screen components

--- app/src/main/java/com/aidos/roomy_app/ui/common_ui/LoginFragment.kt
+++ app/src/main/java/com/aidos/roomy_app/ui/common_ui/LoginFragment.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.aidos.roomy_app.R
 import com.aidos.roomy_app.activities.MainViewModel
+import com.aidos.roomy_app.databinding.FragmentLoginBinding
+import com.aidos.roomy_app.ui.theme.RoomyMainTheme
 import dagger.android.support.DaggerFragment
 import javax.inject.Inject
 
@@ -15,11 +17,10 @@ class LoginFragment : DaggerFragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
-
+    private var binding: FragmentLoginBinding? = null
     private val mainViewModel: LoginViewModel by lazy {
         ViewModelProvider(this, viewModelFactory)[LoginViewModel::class.java]
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,6 +30,14 @@ class LoginFragment : DaggerFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val binding = binding ?: return
+
+        binding.loginComposeView.setContent {
+            RoomyMainTheme {
+
+            }
+        }
     }
 
 }
