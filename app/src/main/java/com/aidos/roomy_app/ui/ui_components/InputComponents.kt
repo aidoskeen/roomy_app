f2b56920796026802248d304fc092d666092e422
aidoskeen <aidoskeen@gmail.com>
2023-03-05 21:35:37 +0200

    DropDownList is added

--- app/src/main/java/com/aidos/roomy_app/ui/ui_components/InputComponents.kt
+++ app/src/main/java/com/aidos/roomy_app/ui/ui_components/InputComponents.kt
@@ -1,29 +1,13 @@
 package com.aidos.roomy_app.ui.ui_components
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 //Field for getting input from user
 @Composable
@@ -41,4 +25,30 @@ fun InputField(
         onValueChange = { onValueChange(value) },
         visualTransformation = visualTransformation
     )
+}
+
+@Composable
+fun DropDownList(
+    expanded: Boolean = false,
+    items: List<String>,
+    onDismissRequest: (Boolean) -> Unit,
+    onItemChosen: (String) -> Unit
+) {
+    DropdownMenu(
+        modifier = Modifier.fillMaxWidth(),
+        expanded = expanded,
+        onDismissRequest = { onDismissRequest(false) },
+    ) {
+        items.forEach {
+            DropdownMenuItem(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    onDismissRequest(false)
+                    onItemChosen(it)
+                }
+            ) {
+                Text(it, modifier = Modifier.wrapContentWidth())
+            }
+        }
+    }
 }
